f3f1463ca044feeec884b716d6691f04d68ac917
aalto95 <43157835+aalto95@users.noreply.github.com>
2026-04-13 17:18:06 +0900

    Fetch schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { type IPage, Renderer, SduiProvider } from "fable-ui";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router";
+
 function App() {
-  return <div>Hello</div>;
+  const [ui, setUi] = useState<IPage[]>([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_SDUI_ORCHESTRATOR_URL}/ui`)
+      .then((response) => response.json())
+      .then((data) => setUi(data.ui))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  return (
+    <SduiProvider>
+      <div className="flex flex-col h-screen items-center justify-center">
+        <div className="flex-1 max-w-7xl w-full">
+          <div className="flex items-center justify-between border-b py-4">
+            <h1 className="text-2xl font-bold">SDUI Example</h1>
+          </div>
+          <Routes>
+            {ui.map((component: IPage) => (
+              <Route
+                key={component.route}
+                path={component.route}
+                element={<Renderer ui={component.ui} />}
+              />
+            ))}
+          </Routes>
+        </div>
+      </div>
+    </SduiProvider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 
 const root = document.getElementById("root");
+
 if (root) {
   createRoot(root).render(
-    <StrictMode>
+    <BrowserRouter>
       <App />
-    </StrictMode>,
+    </BrowserRouter>,
   );
 }
